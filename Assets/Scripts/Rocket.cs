3cba12474769088a17f41d747d4042d7fb5b63b9
Michael Rosique <michaelrosique@gmail.com>
2019-10-16 09:55:30 -0500

    Finished building rocket, added thrust

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
