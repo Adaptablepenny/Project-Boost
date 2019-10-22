2aa5d0385a7abeadbfb6d22a3a8987e82ca0a70e
Michael Rosique <michaelrosique@gmail.com>
2019-10-22 10:21:45 -0500

    Revert "Removed else statement in Coin.cs unecessary code, added coins to the rest of the levels"
    This reverts commit cdcc3e3ab5daf9e3341279544c746d64080dd114.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -32,7 +32,7 @@ public class Coin : MonoBehaviour
         PickupCoin();
     }
 
-    private void PickupCoin()//Limits the score by detecting only one trigger
+    private void PickupCoin()
     {
         coinTouch += 1;
         if (coinTouch == 1)
@@ -42,7 +42,11 @@ public class Coin : MonoBehaviour
             scoreBoard.AddScore(score);
             Destroy(gameObject);
         }
-    }
+        else
+        {
+            return;
+        }
+    }//Limits the score by detecting only one trigger
 
     void CoinRotation()
     {
